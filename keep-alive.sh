cf8a2670fab0f870e0807afc59c38244bdae4b54
Z User <z@container>
2026-04-08 15:07:36 +0000

    d04cdc91-ceb2-4597-b287-9acbc06a3783

--- keep-alive.sh
+++ keep-alive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /home/z/my-project
 while true; do
-  bun run dev >> /home/z/my-project/dev.log 2>&1
-  sleep 1
+  bun run dev 2>&1
+  echo "Server died, restarting in 3s..."
+  sleep 3
 done
